45c1d40215052b1f34544085933a733c8426fbce
Jirawat Chin - on <jirawat.ch@inet.co.th>
2021-02-10 22:08:28 +0700

    Initial Commit

---
+++ test_exe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_exe
+//
+//  Created by Jirawat on 10/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
