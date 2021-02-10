758235e55b72af83b47f0caffecb022269cc7446
Jirawat Chin - on <jirawat.ch@inet.co.th>
2021-02-10 22:15:46 +0700

    swift package

--- test_exe/ViewController/ViewController.swift
+++ test_exe/ViewController/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var btnClick: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testLabel.text = "click"
     }
 
-
+    @IBAction func click_action(_ sender: Any) {
+        testLabel.text = "click Done"
+    }
+    
 }
 
