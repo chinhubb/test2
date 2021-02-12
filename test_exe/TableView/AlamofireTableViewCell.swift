0c4760e5c75fc71ed0976e3e28e7da69a0b10fb9
Jirawat Chin - on <jirawat.ch@inet.co.th>
2021-02-12 21:58:25 +0700

    create tableview

---
+++ test_exe/TableView/AlamofireTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlamofireTableViewCell.swift
+//  test_exe
+//
+//  Created by Jirawat on 12/2/2564 BE.
+//
+
+import UIKit
+
+class AlamofireTableViewCell: UITableViewCell {
+    static let identifier = "AlamofireTableViewCell"
+    static let nib = UINib(nibName: AlamofireTableViewCell.identifier, bundle: nil)
+
+    @IBOutlet var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func setCell() {
+        nameLabel.textColor = UIColor.blue
+    }
+}
--- test_exe/ViewController/AlmofireListViewController.swift
+++ test_exe/ViewController/AlmofireListViewController.swift
@@ -10,8 +10,44 @@ import UIKit
 
 class AlmofireListViewController: UIViewController {
     static let identifier = "AlmofireListViewController"
+    @IBOutlet var tableView: UITableView!
+
     override func viewDidLoad() {
-//        initcomponent()
+        initTableView()
+        initcomponent()
     }
 
+    func initTableView() {
+        tableView.register(AlamofireTableViewCell.nib, forCellReuseIdentifier: AlamofireTableViewCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    func initcomponent() {
+        let newBtn = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(goBack))
+        navigationItem.leftItemsSupplementBackButton = true
+        navigationItem.leftBarButtonItem = newBtn
+    }
+
+    @objc func goBack() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AlmofireListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AlamofireTableViewCell.identifier, for: indexPath) as? AlamofireTableViewCell else { return UITableViewCell() }
+        cell.setCell()
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if let cell = tableView.cellForRow(at: indexPath) {
+//            cell.contentView.backgroundColor = UIColor.black
+//        }
+    }
 }
